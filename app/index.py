fedc6dcb41d65af204001c9429d0b4e55e98b7b4
Chukwunazaekpere <emmanuelchinaza5777@gmail.com>
2022-08-16 16:27:04 +0100

    auth stage

--- app/index.py
+++ app/index.py
@@ -1,11 +1,9 @@
 import os
-# from server import digiext_server
-import server
+from .server import digiext_server
 
 DEBUG = os.getenv("DEBUG")
 PORT = os.getenv("PORT")
 
-digiext_server = server.digiext_server
 digiext_server.run(
     debug=DEBUG,
     port=PORT
