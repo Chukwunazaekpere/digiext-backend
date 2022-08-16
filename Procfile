bf8ef349a04eed9905169baaa50cba23c4c52af8
Chukwunazaekpere <emmanuelchinaza5777@gmail.com>
2022-08-16 19:54:26 +0100

    auth stage

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn src.run.wsgi:digiext_server --timeout 120 --log-file -
+web: gunicorn src.wsgi:digiext_server --timeout 120 --log-file -
--- src/wsgi.py
+++ src/wsgi.py
@@ -1,12 +1,12 @@
 import os
-# from server import digiext_server
-import server
+from .server import digiext_server
 
 DEBUG = os.getenv("DEBUG")
 PORT = os.getenv("PORT")
 
-digiext_server = server.digiext_server
-digiext_server.run(
-    debug=DEBUG,
-    port=PORT
-)
+
+if __name__ == "__main__":
+    digiext_server.run(
+        debug=DEBUG,
+        port=PORT
+    )
