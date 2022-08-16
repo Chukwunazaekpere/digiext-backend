a9af4ae939f253ac3eae0c774e1b340694aa48e4
Chukwunazaekpere <emmanuelchinaza5777@gmail.com>
2022-08-16 16:21:46 +0100

    auth stage

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,9 +1,11 @@
 import os
-from server import digiext_server
+# from server import digiext_server
+import server
 
 DEBUG = os.getenv("DEBUG")
 PORT = os.getenv("PORT")
 
+digiext_server = server.digiext_server
 digiext_server.run(
     debug=DEBUG,
     port=PORT
