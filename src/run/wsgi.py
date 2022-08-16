cbee4005f47a65f232007b008c0aab87fe44b59f
Chukwunazaekpere <emmanuelchinaza5777@gmail.com>
2022-08-16 18:34:33 +0100

    auth stage

--- src/run/wsgi.py
+++ src/run/wsgi.py
@@ -4,8 +4,7 @@ from .server import digiext_server
 DEBUG = os.getenv("DEBUG")
 PORT = os.getenv("PORT")
 
-if __name__ == "__main__":
-    digiext_server.run(
-        debug=DEBUG,
-        port=PORT
-    )
+digiext_server.run(
+    debug=DEBUG,
+    port=PORT
+)
