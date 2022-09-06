7b6a47fe2a81786ca3c404e5ab90759381b25576
Chukwunazaekpere <emmanuelchinaza5777@gmail.com>
2022-09-06 13:08:02 +0100

    update

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -5,8 +5,10 @@ import logging
 
 def database_connection():
     try:
+        logging.basicConfig(level=logging.INFO)
         logging.info("Digiext is initiating a databse connection...")
         DB_URL = os.getenv("DB_URL")
+        # print()
         db_client = MongoClient(host=DB_URL)
         digiext_db = db_client['digiext_db']
         logging.info("Digiext successfully connected to the databse...")
